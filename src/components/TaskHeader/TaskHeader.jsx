a1960196a88970d712ef9b6b98c9db85534e6e4e
pablorenan-dev <pablonichterwistz@outlook.com>
2024-09-22 18:34:23 -0300

    criado esqueleto do html

---
+++ src/components/TaskHeader/TaskHeader.jsx
@@ -0,0 +1,13 @@
+import "./TaskHeaderStyle.css";
+
+const TaskHeader = () => {
+  return (
+    <div>
+      <h2>Create Task</h2>
+      <input type="text" />
+      <button>Clear All</button>
+    </div>
+  );
+};
+
+export default TaskHeader;
